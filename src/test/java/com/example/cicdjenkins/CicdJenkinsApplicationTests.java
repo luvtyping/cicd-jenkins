1660228b257c4a1222de259c942cbb49620a5925
luvtyping <babushkinnavi.ib@gmail.com>
2022-06-11 14:48:54 +0300

    Initial commit

---
+++ src/test/java/com/example/cicdjenkins/CicdJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
