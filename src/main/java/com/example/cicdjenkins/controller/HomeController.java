8c29e08bd1ed280c11b4ebd2d7748ce9a47ef623
Ivan Babushkin <70066684+luvtyping@users.noreply.github.com>
2022-06-13 18:37:16 +0300

    Dev (#5)
    * Fix test
    
    * Fix
    
    * Fix
    
    * Change pom.xml
    
    * Change server port to 8085
    
    * Change server port to 8081
    
    * Change home Url
    
    * Change packaging to jar
    
    * Change packaging to war, update tomcat version to 9.0.64

--- src/main/java/com/example/cicdjenkins/controller/HomeController.java
+++ src/main/java/com/example/cicdjenkins/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public ResponseEntity<String> getHello(@RequestParam(required = false) String name) {
         String s = name != null ? name : "guest";
         return new ResponseEntity<>("Hello, " + s + "!!", HttpStatus.OK);
