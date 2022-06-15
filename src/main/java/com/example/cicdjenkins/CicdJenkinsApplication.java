2fbda75fa3eac8ab1dba00e303753c929383556e
luvtyping <babushkinnavi.ib@gmail.com>
2022-06-15 21:36:46 +0300

    Create deployable war file

--- src/main/java/com/example/cicdjenkins/CicdJenkinsApplication.java
+++ src/main/java/com/example/cicdjenkins/CicdJenkinsApplication.java
@@ -2,9 +2,16 @@ package com.example.cicdjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdJenkinsApplication {
+public class CicdJenkinsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CicdJenkinsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CicdJenkinsApplication.class, args);
